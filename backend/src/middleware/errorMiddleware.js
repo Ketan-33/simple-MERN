264ce2f23d8df12ff473d007001a665dc0383776
ketan <ketanpardhi04@gmail.com>
2024-05-09 02:59:24 +0530

    fresh one

--- backend/src/middleware/errorMiddleware.js
+++
@@ -1,7 +0,0 @@
-// errorMiddleware.js
-const handleErrors = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Internal server error' });
-};
-
-module.exports = { handleErrors };
--- frontend/src/utils/api.js
+++
@@ -1,3 +0,0 @@
-// Define API endpoints and functions for making API requests
-const BASE_URL = 'http://localhost:3000/api'; // Update with your API base URL
-
